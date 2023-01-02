a0077001f1c9dbc23709b7668f85cdddc1d1fccf
Vincent Papelard <papelardvincent@gmail.com>
2023-01-02 15:12:58 +0100

    Fin de get_books_info (manque la sauvegarde dans une BDD Microsoft SQL Server)

--- get_books_info.py
+++ get_books_info.py
@@ -2,23 +2,6 @@ import requests
 import pandas as pd
 from bs4 import BeautifulSoup
 
-def get_book_details(html):
-	"""Take the HTML code for a single book's details
-	and returns a dictionary with all the info we are 
-	looking for about the book"""
-	return
-
-def scrape_page(html):
-	"""Scrapes a whole result page and returns a DataFrame
-	where each line contains all the information regarding 
-	a book"""
-
-	soup = BeautifulSoup(html)
-
-	for book in soup.find_all("article", {"class": "product_pod"}):
-		pass
-	return True
-
 # The base URL to scrape at
 url = "https://books.toscrape.com/catalogue/"
 
@@ -26,6 +9,77 @@ url = "https://books.toscrape.com/catalogue/"
 # If this value is higher than the number of pages, it will
 # scrape everything
 pages_to_scrape = 100
+
+
+def get_book_details(soup):
+	"""Takes the HTML code for a single book's details
+	and returns a dictionary with all the info we are 
+	looking for about the book"""
+
+	# The dictionary that will contain the book's details
+	book_details = {}
+
+	book_details["title"] = soup.find_all("a")[-1]["title"]
+	book_details["uk price"] = soup.find("p", {"class": "price_color"}).text
+
+	if soup.find("p", {"class": "star-rating One"}) : 
+		book_details["rating"] = 1
+	elif soup.find("p", {"class": "star-rating Two"}) : 
+		book_details["rating"] = 2
+	elif soup.find("p", {"class": "star-rating Three"}) : 
+		book_details["rating"] = 3
+	elif soup.find("p", {"class": "star-rating Four"}) : 
+		book_details["rating"] = 4
+	elif soup.find("p", {"class": "star-rating Five"}) : 
+		book_details["rating"] = 5
+
+	book_details["availability"] = soup.find("p", {"class": "instock availability"}).text.strip()
+	book_details["image url"] = soup.find("img")["src"]
+
+	return book_details
+
+def scrape_page(html):
+	"""Scrapes a whole result page and returns a DataFrame
+	where each line contains all the information regarding 
+	a book"""
+
+	soup = BeautifulSoup(html, "lxml")
+	# A list of dictionaries that contains all books' information
+	books = []
+
+	for book in soup.find_all("article", {"class": "product_pod"}):
+		book_info = get_book_details(book)
+		books.append(book_info)
+
+	return pd.DataFrame(books)
+
+def clean_dataframe(df):
+	"""Some columns in the dataframe need to be formatted. The values 
+	in the price column contain an extra 'Â' at the beginning of
+	each value, and the image url is a relative link. This function
+	takes care of these issues"""
+
+	print("Data before cleaning:")
+	print(df.info())
+	print(df.head())
+
+	# We remove the extra 'A' at the beginning of the prices. Moreover,
+	# we remove the currency symbols as all prices are in UK pounds
+	df["uk price"] = df["uk price"].str[2:]
+
+	# As for the image url column, we replace the .. at the beginning
+	# of each link by the full internet address
+	df["image url"] = df["image url"].str[2:]
+	df["image url"] = "https://books.toscrape.com" + df["image url"]
+
+	print("Data after cleaning:")
+	print(df.info())
+	print(df.head())
+
+	return df
+
+# This dataframe will store all the book details
+books_df = pd.DataFrame()
 
 for i in range(1, pages_to_scrape+1):
 
@@ -36,5 +90,10 @@ for i in range(1, pages_to_scrape+1):
 	if not response.ok: break
 
 	page_df = scrape_page(response.text)
+	books_df = pd.concat([books_df, page_df])
 	print(f"{i} catalogue pages processed")
+
+clean_df = clean_dataframe(books_df)
+clean_df.to_csv("books_info.csv")
+
 
